089acf7d1e5b1fae88981ffdab250760641e7b34
Denis <web@dgladkov.ru>
2021-03-27 10:31:49 +0300

    add options page 1

--- admin_option_page/admin/partials/options_page_zw-admin-display.php
+++ admin_option_page/admin/partials/options_page_zw-admin-display.php
@@ -11,6 +11,15 @@
  * @package    Options_page_zw
  * @subpackage Options_page_zw/admin/partials
  */
+
+/**
+	 * Render the options page for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function display_options_page() {
+		include_once 'partials/outdated-notice-admin-display.php';
+	}
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
